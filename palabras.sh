f69a17358b33a02c6ab63c448093c6a86076f648
lumenth56 <sergiopc27.05.05@gmail.com>
2026-09-17 10:25:17 -0600

    feat(palabras.sh) Agrego nuevo argumento

--- palabras.sh
+++ palabras.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if [ $# -eq 0] || [ $# -gt 2 ]; then
     echo "ERROR, colocaste menos de un argumento o más de dos"
+palabra=$1
 carpetas=${1:-/}
-resultados=$(grep -rl -D skip "home" "$carpetas" 2>/dev/null)
+resultados=$(grep -rl -D skip "$palabra" "$carpetas" 2>/dev/null)
 
 if [ -z "$resultados" ]; then
     echo "No se hallo la palabra home"
