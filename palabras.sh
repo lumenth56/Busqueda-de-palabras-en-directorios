3194b43a381210146ea75f486054385c4427c298
lumenth56 <sergiopc27.05.05@gmail.com>
2026-09-17 10:30:24 -0600

    fix(palabras.sh) Error de sintaxis

--- palabras.sh
+++ palabras.sh
@@ -2,7 +2,7 @@
 if [ $# -eq 0] || [ $# -gt 2 ]; then
     echo "ERROR, colocaste menos de un argumento o más de dos"
 palabra=$1
-carpetas=${1:-/}
+carpetas=${2:-/}
 resultados=$(grep -rl -D skip "$palabra" "$carpetas" 2>/dev/null)
 
 if [ -z "$resultados" ]; then
