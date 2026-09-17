2d7ff4aa435097f29ec6086e423ac2cf6caf50ca
lumenth56 <sergiopc27.05.05@gmail.com>
2026-09-17 10:34:39 -0600

    feat(palabras.sh)Añadir nueva salida con la palabra seleccionada

--- palabras.sh
+++ palabras.sh
@@ -6,10 +6,10 @@ carpetas=${2:-/}
 resultados=$(grep -rl -D skip "$palabra" "$carpetas" 2>/dev/null)
 
 if [ -z "$resultados" ]; then
-    echo "No se hallo la palabra home"
+    echo "No se hallo la palabra $palabra"
     exit 0
 fi
-echo "Archivos con home":
+echo "Archivos con $palabra":
 echo "$resultados" | while read -r archivo; do
     realpath "$archivo"
 done
